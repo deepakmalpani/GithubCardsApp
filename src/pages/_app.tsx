c583b87f0aeb6e04e173507cb39c741580177669
Deepak Malpani <deepak.malpani@exxonmobil.com>
2023-04-09 18:53:52 +0530

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return <Component title = "Github cards app" />
 }
+
+// class App extends React.Component<{title : string}, {}>{
+
+//   render() {
+//     return <div className='header'>{this.props.title}</div>
+//   }
+// }
+
+// // export default App;
+// ReactDOM.render(
+//   <App title = "The Github Cards App" />,
+//   app,
+// );
